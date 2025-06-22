3a7c105e71949942ecc65ae0a9c9882aaafdeff7
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-06-23 03:37:49 +0600

    using class and id name

--- tour of dom/js/index.js
+++ tour of dom/js/index.js
@@ -3,11 +3,19 @@ let liCollection = document.getElementsByTagName('li');
 // console.log(liCollection);
 for(let li of liCollection){
     let liText = li.innerText;
-    console.log(liText);
+    // console.log(liText);
 }
 
 let allHeadings = document.getElementsByTagName('h1');
 for(let heading of allHeadings){
     let headingText = heading.innerText;
-    console.log(headingText);
+    // console.log(headingText);
+}
+
+let title = document.getElementById('fruits-title');
+title.innerText = 'Fruits title change by Js'
+
+let names = document.getElementsByClassName('fruits-name')
+for(let name of names){
+    console.log(name.innerText)
 }
