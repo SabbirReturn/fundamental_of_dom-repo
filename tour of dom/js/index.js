2074520ef6f40fe7da4fd7eb061176b8743270d8
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-06-23 03:19:45 +0600

    using getElementByTagName

--- tour of dom/js/index.js
+++ tour of dom/js/index.js
@@ -1,1 +1,13 @@
-console.log(document.body)
+// console.log(document.body)
+let liCollection = document.getElementsByTagName('li');
+// console.log(liCollection);
+for(let li of liCollection){
+    let liText = li.innerText;
+    console.log(liText);
+}
+
+let allHeadings = document.getElementsByTagName('h1');
+for(let heading of allHeadings){
+    let headingText = heading.innerText;
+    console.log(headingText);
+}
